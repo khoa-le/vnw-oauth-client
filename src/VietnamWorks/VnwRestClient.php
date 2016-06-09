c282eb36ee4f2ff26364804c4b0c9410dac71dc5
Khoa, Le Ngoc <khoaln6@gmail.com>
2016-06-09 10:36:36 +0700

    update webservice composer

--- src/VietnamWorks/VnwRestClient.php
+++ src/VietnamWorks/VnwRestClient.php
@@ -9,8 +9,11 @@
 namespace VietnamWorks;
 
 
+use GuzzleHttp\Psr7\MultipartStream;
 use GuzzleHttp\Psr7\Request;
+use Http\Client\HttpClient;
 use Http\Discovery\HttpClientDiscovery;
+use Psr\Http\Message\ResponseInterface;
 
 class VnwRestClient
 {
@@ -41,22 +44,61 @@ class VnwRestClient
      * If we should use SSL or not
      * @var bool
      */
-    protected $sslEnabled = true;
+    protected $sslEnabled = false;
 
-    public function __construct($apiKey, $apiSecret,$apiHost, HttpClient $httpClient = null)
+    protected $accessToken = null;
+
+    public function __construct($apiKey, $apiSecret, $apiHost, HttpClient $httpClient = null)
     {
         $this->apiKey = $apiKey;
         $this->apiSecret = $apiSecret;
         $this->apiHost = $apiHost;
-        $this->httpClient = new $httpClient;
+        $this->httpClient = $httpClient;
     }
 
-    public function send($method, $uri, $body, $headers)
+    public function send($method, $uri, $accessToken='', $body = null, $headers = array(),$files=array())
     {
+        if (!empty($accessToken)) {
+            $headers['Authorization'] = 'Bearer '. $accessToken;
+        }
+
+        if (!empty($files)) {
+            $body = new MultipartStream($files);
+            $headers['Content-Type'] = 'multipart/form-data; boundary='.$body->getBoundary();
+        }
+
         $request = new Request($method, $this->getApiUrl($uri), $headers, $body);
         $response = $this->getHttpClient()->sendRequest($request);
-        var_dump($response);die;
+
+        return $this->responseHandler($response);
     }
+
+    public function get($endpointUrl, $accessToken = '' ,$queryString = array())
+    {
+        return $this->send('GET',$endpointUrl.'?'.http_build_query($queryString) , $accessToken);
+    }
+
+    public function post($endpointUrl, $accessToken, $postData = array(), $files = array())
+    {
+        $postDataMultipart = [];
+        $postDataMultipart[]=array(
+            'name'=>'json',
+            'contents'=>json_encode($postData)
+        );
+
+
+        $body['json'] = json_encode($postData);
+        if (!empty($files)) {
+            $postDataMultipart[]=array(
+                'name'=>$files['name'],
+                'contents'=> fopen($files['file'], 'r'),
+                'filename'=>$files['file_name']
+            );
+        }
+
+        return $this->send('POST', $endpointUrl, $accessToken, [],[],$postDataMultipart);
+    }
+
 
     /**
      *
@@ -81,33 +123,6 @@ class VnwRestClient
 
 
     /**
-     * @param $uri
-     *
-     * @return string
-     */
-    private function getApiUrl($uri)
-    {
-        return $this->generateEndpoint($this->apiHost, $this->apiVersion, $this->sslEnabled).$uri;
-    }
-
-
-    /**
-     * @param string $apiEndpoint
-     * @param string $apiVersion
-     * @param bool   $ssl
-     *
-     * @return string
-     */
-    private function generateEndpoint($apiEndpoint, $apiVersion, $ssl)
-    {
-        if (!$ssl) {
-            return 'http://'.$apiEndpoint.'/'.$apiVersion.'/';
-        } else {
-            return 'https://'.$apiEndpoint.'/'.$apiVersion.'/';
-        }
-    }
-
-    /**
      * @param string $apiVersion
      *
      * @return RestClient
@@ -129,6 +144,49 @@ class VnwRestClient
         $this->sslEnabled = $sslEnabled;
 
         return $this;
+    }
+
+
+    /**
+     * @param $uri
+     *
+     * @return string
+     */
+    private function getApiUrl($uri)
+    {
+        return $this->generateEndpoint($this->apiHost, $this->apiVersion, $this->sslEnabled).$uri;
+    }
+
+
+    /**
+     * @param string $apiEndpoint
+     * @param string $apiVersion
+     * @param bool $ssl
+     *
+     * @return string
+     */
+    private function generateEndpoint($apiEndpoint, $apiVersion, $ssl)
+    {
+        if (!$ssl) {
+            return 'http://'.$apiEndpoint.'/';
+        } else {
+            return 'https://'.$apiEndpoint.'/';
+        }
+    }
+
+
+    private function responseHandler(ResponseInterface $response)
+    {
+        $httpResponseCode = $response->getStatusCode();
+        $result = new \stdClass();
+        $data = (string)$response->getBody();
+        $jsonResponseData = json_decode($data, false);
+
+        // return response data as json if possible, raw if not
+        $result->http_response_body = $data && $jsonResponseData === null ? $data : $jsonResponseData;
+        $result->http_response_code = $httpResponseCode;
+
+        return $result;
     }
 
 }

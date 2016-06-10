f30ed72964fbc313fc42f03555c59a848dfafb87
Khoa, Le Ngoc <khoaln6@gmail.com>
2016-06-10 15:59:24 +0700

    update code for repository

---
+++ src/Vietnamworks/VietnamWorks/Constants/ExceptionMessages.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace VietnamWorks\Constants;
+
+
+class ExceptionMessages {
+    const EXCEPTION_INVALID_CREDENTIALS = "Your credentials are incorrect.";
+    const EXCEPTION_GENERIC_HTTP_ERROR = "An HTTP Error has occurred! Check your network connection and try again.";
+    const EXCEPTION_MISSING_REQUIRED_PARAMETERS = "The parameters passed to the API were invalid. Check your inputs!";
+    const EXCEPTION_MISSING_REQUIRED_MIME_PARAMETERS = "The parameters passed to the API were invalid. Check your inputs!";
+    const EXCEPTION_MISSING_ENDPOINT = "The endpoint you've tried to access does not exist. Check your URL.";
+}
+ 
